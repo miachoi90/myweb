b98ed4687b8761b5edbb63378f905d6efdf45ddd
minimia90 <minimia.choi@gmail.com>
2022-08-27 00:36:44 -0700

    init

---
+++ js/script.js
@@ -0,0 +1,265 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	$('#wrap').scroll(function(event) {
+		var x = $(this).scrollLeft();
+		
+		if ( x<= 9 ) {
+			
+			
+			$('#shp1').removeClass('animate').addClass('animate0');
+			$('#shp1p').removeClass('animate').addClass('animate0');
+			$('#shp2').removeClass('animate').addClass('animate0');
+			$('#shp2p').removeClass('animate').addClass('animate0');
+	        $('#shp3').removeClass('animate').addClass('animate0');
+			$('#shp3p').removeClass('animate').addClass('animate0');
+			$('#shp4').removeClass('animate').addClass('animate0');
+			$('#shp4p').removeClass('animate').addClass('animate0');
+			$('#shp5').removeClass('animate').addClass('animate0');
+			$('#shp5p').removeClass('animate').addClass('animate0');
+		    $('#shp6').removeClass('animate').addClass('animate0');
+			$('#shp6p').removeClass('animate').addClass('animate0');
+			$('#shp7').removeClass('animate').addClass('animate0');
+			$('#shp7p').removeClass('animate').addClass('animate0');
+		
+	
+
+	$('#shp1').removeClass('animate4');	$('#shp1p').removeClass('animate4');
+    $('#shp1').removeClass('animate3');	$('#shp1p').removeClass('animate3');
+	$('#shp1').removeClass('animate2');	$('#shp1p').removeClass('animate2');
+	
+	
+	$('#shp2').removeClass('animate4');	$('#shp2p').removeClass('animate4');
+    $('#shp2').removeClass('animate3');	$('#shp2p').removeClass('animate3');
+	$('#shp2').removeClass('animate2');	$('#shp2p').removeClass('animate2');
+			
+
+	$('#shp3').removeClass('animate4');	$('#shp3p').removeClass('animate4');
+    $('#shp3').removeClass('animate3');	$('#shp3p').removeClass('animate3');
+	$('#shp3').removeClass('animate2');	$('#shp3p').removeClass('animate2');
+					 				 $('#fl').removeClass('animate4');
+
+	} else if(x >= 50 && x <= 1500) {
+		
+		
+			$('#shp1').removeClass('animate0').addClass('animate');
+			$('#shp1p').removeClass('animate0').addClass('animate');
+	
+			
+			$('#shp2').removeClass('animate0').addClass('animate');
+			$('#shp2p').removeClass('animate0').addClass('animate');
+			
+	
+			$('#shp3').removeClass('animate0').addClass('animate');
+			$('#shp3p').removeClass('animate0').addClass('animate');
+			
+	
+			$('#shp4').removeClass('animate0').addClass('animate');
+			$('#shp4p').removeClass('animate0').addClass('animate');
+			
+
+			$('#shp5').removeClass('animate0').addClass('animate');
+			$('#shp5p').removeClass('animate0').addClass('animate');
+			
+		
+			$('#shp6').removeClass('animate0').addClass('animate');
+			$('#shp6p').removeClass('animate0').addClass('animate');
+		
+			
+			$('#shp7').removeClass('animate0').addClass('animate');
+			$('#shp7p').removeClass('animate0').addClass('animate');
+	$('#shp1').removeClass('animate4');	$('#shp1p').removeClass('animate4');
+    $('#shp1').removeClass('animate3');	$('#shp1p').removeClass('animate3');
+	$('#shp1').removeClass('animate2');	$('#shp1p').removeClass('animate2');
+	
+	
+	$('#shp2').removeClass('animate4');	$('#shp2p').removeClass('animate4');
+    $('#shp2').removeClass('animate3');	$('#shp2p').removeClass('animate3');
+	$('#shp2').removeClass('animate2');	$('#shp2p').removeClass('animate2');
+			
+
+	$('#shp3').removeClass('animate4');	$('#shp3p').removeClass('animate4');
+    $('#shp3').removeClass('animate3');	$('#shp3p').removeClass('animate3');
+	$('#shp3').removeClass('animate2');	$('#shp3p').removeClass('animate2');
+		
+		$('#shp4').removeClass('animate4');	$('#shp4p').removeClass('animate4');
+    $('#shp4').removeClass('animate3');	$('#shp4p').removeClass('animate3');
+	$('#shp4').removeClass('animate2');	$('#shp4p').removeClass('animate2');	
+	
+		$('#shp5').removeClass('animate4');	$('#shp5p').removeClass('animate4');
+    $('#shp5').removeClass('animate3');	$('#shp5p').removeClass('animate3');
+	$('#shp5').removeClass('animate2');	$('#shp5p').removeClass('animate2');
+	
+			$('#shp6').removeClass('animate4');	$('#shp6p').removeClass('animate4');
+    $('#shp6').removeClass('animate3');	$('#shp6p').removeClass('animate3');
+	$('#shp6').removeClass('animate2');	$('#shp6p').removeClass('animate2');
+		
+			$('#shp7').removeClass('animate4');	$('#shp7p').removeClass('animate4');
+    $('#shp7').removeClass('animate3');	$('#shp7p').removeClass('animate3');
+	$('#shp7').removeClass('animate2');	$('#shp7p').removeClass('animate2');
+	
+		 $('#fl').removeClass('animate4');
+			 } else if(x >= 1550 && x <=2500) { 
+			 $('#shp1').removeClass('animate').addClass('animate2');
+			$('#shp1p').removeClass('animate').addClass('animate2');
+			
+			$('#shp2').removeClass('animate').addClass('animate2');
+			$('#shp2p').removeClass('animate').addClass('animate2');
+			
+			$('#shp3').removeClass('animate').addClass('animate2');
+			$('#shp3p').removeClass('animate').addClass('animate2');
+			 
+			$('#shp4').removeClass('animate').addClass('animate2');
+			$('#shp4p').removeClass('animate').addClass('animate2');
+			
+			$('#shp5').removeClass('animate').addClass('animate2');
+			$('#shp5p').removeClass('animate').addClass('animate2');
+			 
+			$('#shp6').removeClass('animate').addClass('animate2');
+			$('#shp6p').removeClass('animate').addClass('animate2');
+			
+			$('#shp7').removeClass('animate').addClass('animate2');
+			$('#shp7p').removeClass('animate').addClass('animate2');
+			
+			 $('#fl').removeClass('animate4');
+			
+		
+$('#shp4').removeClass('animate4');	$('#shp4p').removeClass('animate4');
+    $('#shp4').removeClass('animate0');	$('#shp4p').removeClass('animate0');
+			$('#shp4').removeClass('animate');	$('#shp4p').removeClass('animate');
+$('#shp5').removeClass('animate4');	$('#shp5p').removeClass('animate4');
+    $('#shp5').removeClass('animate0');	$('#shp5p').removeClass('animate0');
+			$('#shp5').removeClass('animate');	$('#shp5p').removeClass('animate');
+		
+	$('#shp6').removeClass('animate4');	$('#shp6p').removeClass('animate4');
+    $('#shp6').removeClass('animate0');	$('#shp6p').removeClass('animate0');
+			$('#shp6').removeClass('animate');	$('#shp6p').removeClass('animate');
+			
+			$('#shp7').removeClass('animate4');	$('#shp7p').removeClass('animate4');
+    $('#shp7').removeClass('animate0');	$('#shp7p').removeClass('animate0');
+			$('#shp7').removeClass('animate');	$('#shp7p').removeClass('animate');
+							 $('#shp7p').removeClass('animate4_4');
+							 
+							 
+		
+			
+			 } else if (x >= 2800 && x <=3800) {
+			
+			$('#shp1').removeClass('animate2').addClass('animate3');
+			$('#shp1p').removeClass('animate2').addClass('animate3');
+			
+			$('#shp2').removeClass('animate2').addClass('animate3');
+			$('#shp2p').removeClass('animate2').addClass('animate3');
+			
+			$('#shp3').removeClass('animate2').addClass('animate3');
+			$('#shp3p').removeClass('animate2').addClass('animate3');
+			 
+			$('#shp4').removeClass('animate2').addClass('animate3');
+			$('#shp4p').removeClass('animate2').addClass('animate3');
+			
+			$('#shp5').removeClass('animate2').addClass('animate3');
+			$('#shp5p').removeClass('animate2').addClass('animate3');
+			 
+			$('#shp6').removeClass('animate2').addClass('animate3');
+			$('#shp6p').removeClass('animate2').addClass('animate3');
+			
+			$('#shp7').removeClass('animate2').addClass('animate3');
+			$('#shp7p').removeClass('animate2').addClass('animate3');
+			
+	$('#shp1').removeClass('animate4');	$('#shp1p').removeClass('animate4');
+    $('#shp1').removeClass('animate0');	$('#shp1p').removeClass('animate0');
+	$('#shp1').removeClass('animate2');	$('#shp1p').removeClass('animate2');
+		$('#shp1').removeClass('animate');	$('#shp1p').removeClass('animate');
+
+	
+	$('#shp2').removeClass('animate4');	$('#shp2p').removeClass('animate4');
+    $('#shp2').removeClass('animate0');	$('#shp2p').removeClass('animate0');
+	$('#shp2').removeClass('animate2');	$('#shp2p').removeClass('animate2');
+					$('#shp2').removeClass('animate');	$('#shp2p').removeClass('animate');
+
+
+	$('#shp3').removeClass('animate4');	$('#shp3p').removeClass('animate4');
+    $('#shp3').removeClass('animate0');	$('#shp3p').removeClass('animate0');
+	$('#shp3').removeClass('animate2');	$('#shp3p').removeClass('animate2');
+			$('#shp3').removeClass('animate');	$('#shp3p').removeClass('animate');
+
+
+		
+$('#shp4').removeClass('animate4');	$('#shp4p').removeClass('animate4');
+    $('#shp4').removeClass('animate0');	$('#shp4p').removeClass('animate0');
+	$('#shp4').removeClass('animate2');	$('#shp4p').removeClass('animate2');
+			$('#shp4').removeClass('animate');	$('#shp4p').removeClass('animate');
+$('#shp5').removeClass('animate4');	$('#shp5p').removeClass('animate4');
+    $('#shp5').removeClass('animate0');	$('#shp5p').removeClass('animate0');
+	$('#shp5').removeClass('animate2');	$('#shp5p').removeClass('animate2');
+			$('#shp5').removeClass('animate');	$('#shp5p').removeClass('animate');
+		
+	$('#shp6').removeClass('animate4');	$('#shp6p').removeClass('animate4');
+    $('#shp6').removeClass('animate0');	$('#shp6p').removeClass('animate0');
+	$('#shp6').removeClass('animate2');	$('#shp6p').removeClass('animate2');
+			$('#shp6').removeClass('animate');	$('#shp6p').removeClass('animate');
+			
+			$('#shp7').removeClass('animate4');	$('#shp7p').removeClass('animate4');
+    $('#shp7').removeClass('animate0');	$('#shp7p').removeClass('animate0');
+	$('#shp7').removeClass('animate2');	$('#shp7p').removeClass('animate2');
+			$('#shp7').removeClass('animate');	$('#shp7p').removeClass('animate');
+							 $('#shp7p').removeClass('animate4_4');
+
+			 $('#fl').removeClass('animate4');
+			
+			 } else if (x >= 4500 && x <=5000) {
+				 $('#shp1').removeClass('animate3').addClass('animate4');
+				 $('#shp1p').removeClass('animate3').addClass('animate4');
+				  $('#shp2').removeClass('animate3').addClass('animate4');
+				 $('#shp2p').removeClass('animate3').addClass('animate4');
+				   $('#shp3').removeClass('animate3').addClass('animate4');
+				 $('#shp3p').removeClass('animate3').addClass('animate4');
+                 $('#shp4').removeClass('animate3').addClass('animate4');
+				 $('#shp4p').removeClass('animate3').addClass('animate4');
+                 $('#shp5').removeClass('animate3').addClass('animate4');
+				 $('#shp5p').removeClass('animate3').addClass('animate4');
+                 $('#shp6').removeClass('animate3').addClass('animate4');
+				 $('#shp6p').removeClass('animate3').addClass('animate4');
+                 $('#shp7').removeClass('animate3').addClass('animate4');
+				 $('#shp7p').removeClass('animate3').addClass('animate4');
+				 $('#shp7p').addClass('animate4_4');
+				 				 $('#fl').addClass('animate4');
+
+
+
+    $('#shp1').removeClass('animate');	$('#shp1p').removeClass('animate');
+    $('#shp1').removeClass('animate3');	$('#shp1p').removeClass('animate3');
+	$('#shp1').removeClass('animate2');	$('#shp1p').removeClass('animate2');
+	
+	
+	$('#shp2').removeClass('animate');	$('#shp2p').removeClass('animate');
+    $('#shp2').removeClass('animate3');	$('#shp2p').removeClass('animate3');
+	$('#shp2').removeClass('animate2');	$('#shp2p').removeClass('animate2');
+			
+
+	$('#shp3').removeClass('animate');	$('#shp3p').removeClass('animate');
+    $('#shp3').removeClass('animate3');	$('#shp3p').removeClass('animate3');
+	$('#shp3').removeClass('animate2');	$('#shp3p').removeClass('animate2');
+
+				 
+		
+    $('#shp4').removeClass('animate');	$('#shp4p').removeClass('animate');
+    $('#shp4').removeClass('animate3');	$('#shp4p').removeClass('animate3');
+	$('#shp4').removeClass('animate2');	$('#shp4p').removeClass('animate2');
+	
+	
+	$('#shp5').removeClass('animate');	$('#shp5p').removeClass('animate');
+    $('#shp5').removeClass('animate3');	$('#shp5p').removeClass('animate3');
+	$('#shp5').removeClass('animate2');	$('#shp5p').removeClass('animate2');
+			
+
+	$('#shp6').removeClass('animate');	$('#shp6p').removeClass('animate');
+    $('#shp6').removeClass('animate3');	$('#shp6p').removeClass('animate3');
+	$('#shp6').removeClass('animate2');	$('#shp6p').removeClass('animate2');	
+	$('#shp7').removeClass('animate');	$('#shp7p').removeClass('animate');
+    $('#shp7').removeClass('animate3');	$('#shp7p').removeClass('animate3');
+	$('#shp7').removeClass('animate2');	$('#shp7p').removeClass('animate2');		 }
+    
+});
+});
+
